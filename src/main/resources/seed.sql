8226b09997f0036e827ac5598e01476de2e78783
Alexandros McCray <223855625+alexm-dev@users.noreply.github.com>
2026-06-22 11:28:28 +0200

    Update category sql seeding

--- src/main/resources/seed.sql
+++ src/main/resources/seed.sql
@@ -2,30 +2,61 @@
 -- Still needs to be discussed to what reference data we want.
 -- Admin panel might be used to add reference data if we want as well.
 
--- Categories
-INSERT INTO categories (name, description)
-SELECT 'Electronics', 'Phones, TVs and other electronics'
-WHERE NOT EXISTS (SELECT 1 FROM categories WHERE name = 'Electronics');
+INSERT OR IGNORE INTO categories (name, description)
+VALUES
+  ('Electronics', 'Phones, TVs and other electronics.'),
+  ('Tools', 'Power tools and hand tools.'),
+  ('Gaming', 'Enter a world of adventure and exitement.'),
+  ('Pets', 'Show your animal friends some love.'),
+  ('Outdoor', 'Get into the great outdoors.'),
+  ('Fashion', 'Find your style.'),
+  ('Travel', 'Discover the world'),
+  ('Home', 'Transform your space into a work of art'),
+  ('Music', 'Turn it up'),
+  ('Designer Goods', 'Surround yourself with luxury'),
+  ('Plants', 'Transform your home into a oasis'),
+  ('Cooking', 'Cook up something special'),
+  ('Toys & Collectibles', 'Bring home a new friend'),
+  ('Driveables', 'Drive your mood'),
+  ('Real Estate', 'Find your new home'),
+  ('Books', 'Read to your desire'),
+  ('Health & Beauty', 'Take care of yourself'),
+  ('Jewelry & Watches', 'Luxury lifestyle'),
+  ('Sporting', 'Experience greatness'),
+  ('Baby', 'Baby supplies'),    
+  ('Everything else', 'Discover hidden gems');
 
-INSERT INTO categories (name, description)
-SELECT 'Tools', 'Power tools and hand tools'
-WHERE NOT EXISTS (SELECT 1 FROM categories WHERE name = 'Tools');
+INSERT OR IGNORE INTO sub_categories (name, category_id)
+SELECT 'Selfphone', id FROM categories WHERE name = 'Electronics';
 
-INSERT INTO sub_categories (name, category_id)
-SELECT 'Smartphone', (SELECT id FROM categories WHERE name = 'Electronics')
-WHERE NOT EXISTS (
-    SELECT 1 FROM sub_categories
-    WHERE name = 'Smartphone'
-      AND category_id = (SELECT id FROM categories WHERE name = 'Electronics')
-);
+INSERT OR IGNORE INTO sub_categories (name, category_id)
+SELECT 'TV', id FROM categories WHERE name = 'Electronics';
 
-INSERT INTO sub_categories (name, category_id)
-SELECT 'TV', (SELECT id FROM categories WHERE name = 'Electronics')
-WHERE NOT EXISTS (
-    SELECT 1 FROM sub_categories
-    WHERE name = 'TV'
-      AND category_id = (SELECT id FROM categories WHERE name = 'Electronics')
-);
+INSERT OR IGNORE INTO sub_categories (name, category_id)
+SELECT 'Laptop', id FROM categories WHERE name = 'Electronics';
+
+INSERT OR IGNORE INTO sub_categories (name, category_id)
+SELECT 'Camera', id FROM categories WHERE name = 'Electronics';
+
+INSERT OR IGNORE INTO sub_categories (name, category_id)
+SELECT 'Smart Watch', id FROM categories WHERE name = 'Electronics';
+
+INSERT OR IGNORE INTO sub_categories (name, category_id)
+SELECT 'Home Electronics', id FROM categories WHERE name = 'Electronics';
+
+
+INSERT OR IGNORE INTO sub_categories (name, category_id)
+SELECT 'Hammer', id FROM categories WHERE name = 'Tools';
+
+INSERT OR IGNORE INTO sub_categories (name, category_id)
+SELECT 'Drill', id FROM categories WHERE name = 'Tools';
+
+
+INSERT OR IGNORE INTO sub_categories (name, category_id)
+SELECT 'Console', id FROM categories WHERE name = 'Gaming';
+
+INSERT OR IGNORE INTO sub_categories (name, category_id)
+SELECT 'Board Games', id FROM categories WHERE name = 'Gaming';
 
 -- Roles
 INSERT INTO roles (name) SELECT 'lender' WHERE NOT EXISTS (SELECT 1 FROM roles WHERE name = 'lender');
