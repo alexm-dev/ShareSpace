e52742cc560434896c3940923783494e6c77a08c
Lazar <246503773+LazarMilosevic@users.noreply.github.com>
2026-06-24 14:14:14 +0200

    added role selection to RegistrationPage.java

--- src/main/java/app/ui/RegistrationPage.java
+++ src/main/java/app/ui/RegistrationPage.java
@@ -1,19 +1,23 @@
 package app.ui;
 
+import app.model.Role;
 import app.model.User;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 
+import java.util.List;
+
 public class RegistrationPage {
 
     public VBox build() {
+
+        List<Role> allRoles = ShareS.userService.getAllRoles();
+        final int renterID = allRoles.stream().filter(r -> r.getName().equals("renter")).findFirst().get().getId();
+        final int lenderId =  allRoles.stream().filter(r -> r.getName().equals("lender")).findFirst().get().getId();
 
         Button back = Ui.button(
                 "← Back",
@@ -26,7 +30,7 @@ public class RegistrationPage {
                 "-fx-background-color: transparent;");
         login.setOnAction(e -> ShareS.showLoginPage());
 
-        //horizontal tabs
+        //horizontal tabs at the top
         HBox htabs = new HBox(
                 20,
                 back,
@@ -42,6 +46,23 @@ public class RegistrationPage {
         PasswordField pw = new PasswordField();
         pw.setPromptText("Password");
 
+        //radioboxes for roles
+        ToggleGroup group = new ToggleGroup();
+        RadioButton roleRenter = new RadioButton("Renter");
+        RadioButton roleLender = new RadioButton("Lender");
+        RadioButton roleBoth = new RadioButton("Both");
+
+        roleRenter.setToggleGroup(group);
+        roleLender.setToggleGroup(group);
+        roleBoth.setToggleGroup(group);
+
+        HBox roles = new HBox(
+                12,
+                roleRenter,
+                roleLender,
+                roleBoth
+        );
+
         Label error = Ui.light("", 12);
         error.setStyle("-fx-text-fill: #e53935;");
 
@@ -53,17 +74,32 @@ public class RegistrationPage {
         registerBtn.setOnAction(e -> {
             if (username.getText().isBlank()) {error.setText("Username can't be blank."); return;}
 
+            RadioButton selected = (RadioButton) group.getSelectedToggle();
+            if (selected == null) {error.setText("Please select a role."); return;}
+
             if (email.getText().isBlank()) {error.setText("Email can't be blank."); return;}
 
             if (pw.getText().isBlank()) {
                 error.setText("Password can't be blank.");
-                pw.clear();
                 return;
             }
+
 
             User user = ShareS.userService.register(username.getText().trim(), email.getText().trim(), pw.getText().toCharArray());
             if (user != null) {
                 ShareS.session.loginAfterRegister(user);
+                switch (selected.getText()) {
+                    case "Renter":
+                        ShareS.userService.assignRoleToUser(user.getId(), renterID);
+                        break;
+                    case "Lender":
+                        ShareS.userService.assignRoleToUser(user.getId(), lenderId);
+                        break;
+                    case "Both":
+                        ShareS.userService.assignRoleToUser(user.getId(), renterID);
+                        ShareS.userService.assignRoleToUser(user.getId(), lenderId);
+                        break;
+                }
                 ShareS.showProfileSettingsPage();
             } else {
                 error.setText("Registration failed.");
@@ -76,6 +112,7 @@ public class RegistrationPage {
         VBox register = new VBox(
                 10,
                 Ui.light("Username", 11), username,
+                Ui.light("Roles", 11), roles,
                 Ui.light("Email address", 11), email,
                 Ui.light("Password", 11), pw,
                 error,
@@ -83,18 +120,18 @@ public class RegistrationPage {
         register.setMaxWidth(360);
 
         //bold + registration tabs
-        VBox createA = new VBox(
+        VBox createAcc = new VBox(
                 24,
                 Ui.bold("Create an account", 56),
                 register);
-        createA.setAlignment(Pos.CENTER);
-        VBox.setVgrow(createA, Priority.ALWAYS);
+        createAcc.setAlignment(Pos.CENTER);
+        VBox.setVgrow(createAcc, Priority.ALWAYS);
 
         //combined layouts
         VBox root = new VBox(
                 0,
                 htabs,
-                createA);
+                createAcc);
         root.setFillWidth(true);
         root.setPadding(new Insets(16, 60, 40, 60));
         root.setStyle("-fx-background-color: white;");
