259ce46920f2c2525b6530a3f26333d387f34524
Alexandros McCray <223855625+alexm-dev@users.noreply.github.com>
2026-06-29 19:12:25 +0200

    Include new Constants util package

---
+++ src/main/java/app/util/Constants.java
@@ -0,0 +1,11 @@
+package app.util;
+
+/**
+ * Constants used throughout the application.
+ */
+public class Constants {
+    /**
+     * Maximum size of an image in bytes (5 MB).
+     */
+    public static final int MAX_IMAGE_BYTES = 5 * 1024 * 1024;
+}
