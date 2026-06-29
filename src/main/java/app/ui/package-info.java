7b4da58b69387db140c4041646042fd4ec2d4cea
Alexandros McCray <223855625+alexm-dev@users.noreply.github.com>
2026-06-29 19:14:56 +0200

    Add UI package info

---
+++ src/main/java/app/ui/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * UI package for ShareSpace.
+ * Handles all user interface components and interactions.
+ *
+ * Interacts with the backend services to display data and receive user input.
+ */
+
+package app.ui;
